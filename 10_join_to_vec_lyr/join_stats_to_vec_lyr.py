6c882f5b4abb994494eeb1c58eece0963dce5be6
Pete Bunting <petebunting@mac.com>
2022-07-19 22:59:21 +0100

    Script to join stats on to the vector layer.

---
+++ 10_join_to_vec_lyr/join_stats_to_vec_lyr.py
@@ -0,0 +1,20 @@
+import geopandas
+import pandas
+
+vec_file = "/Users/pete/Dropbox/University/Research/Projects/GlobalMangroveWatch/202207_ProtectedAreas/WDPA-July22-PA_DEF-STATUS-MANGROVE.gpkg"
+vec_lyr = "WDPA-July22-PA_DEF-STATUS-MANGROVE-2"
+
+wpda_gpdf = geopandas.read_file(vec_file, layer=vec_lyr)
+wpda_gpdf = wpda_gpdf.set_index("WDPAID")
+
+extent_stats_file = "../06_merge_extent_stats/protected_area_v3_corrected_ext_stats.feather"
+agb_hgt_stats_file = "../09_merge_agb_hgt_stats/protected_agb_hgt_summarised_base_stats.feather"
+
+extent_stats_df = pandas.read_feather(extent_stats_file)
+agb_hgt_stats_df = pandas.read_feather(agb_hgt_stats_file)
+
+wpda_gpdf_join = wpda_gpdf.join(extent_stats_df.set_index("WDPAID"), on="WDPAID")
+wpda_gpdf_join = wpda_gpdf_join.join(agb_hgt_stats_df.set_index("WDPAID"), on="WDPAID")
+
+wpda_gpdf_join.to_file("WDPA_July22_PA_DEF_STATUS_MANGROVE_GMW_Stats.gpkg", layer="WDPA_July22_PA_DEF_STATUS_MANGROVE_GMW_Stats", driver="GPKG")
+

219c5096373eace31c00438d573ddb8c6e835ac6
Pete Bunting <petebunting@mac.com>
2022-07-27 10:47:44 +0100

    Add scripts to calculate the tile stats for the soil and total carbon and co2 layers.

---
+++ 10_calc_carbon_stats/01_calc_tile_stats/gen_cmds.py
@@ -0,0 +1,120 @@
+from pbprocesstools.pbpt_q_process import PBPTGenQProcessToolCmds
+
+import logging
+import os
+
+import rsgislib.tools.filetools
+import rsgislib.tools.utils
+import rsgislib.vectorutils
+
+logger = logging.getLogger(__name__)
+
+
+class GenTaskCmds(PBPTGenQProcessToolCmds):
+    def gen_command_info(self, **kwargs):
+        if not os.path.exists(kwargs["out_path"]):
+            os.mkdir(kwargs["out_path"])
+
+        protect_area_lyrs = rsgislib.vectorutils.get_vec_lyrs_lst(
+            kwargs["vec_protect_areas_file"]
+        )
+        tile_lut = rsgislib.tools.utils.read_json_to_dict(kwargs["tile_lut_file"])
+
+        for protect_area_lyr in protect_area_lyrs:
+            protect_area_tiles = tile_lut[protect_area_lyr]
+            out_protect_dir = os.path.join(kwargs["out_path"], protect_area_lyr)
+            if not os.path.exists(out_protect_dir):
+                os.mkdir(out_protect_dir)
+
+            roi_dir = os.path.join(out_protect_dir, "extent")
+            if not os.path.exists(roi_dir):
+                os.mkdir(roi_dir)
+
+            out_dir = os.path.join(out_protect_dir, kwargs["out_dir_name"])
+            if not os.path.exists(out_dir):
+                os.mkdir(out_dir)
+
+            for protect_area_tile in protect_area_tiles:
+                carbon_img_file = rsgislib.tools.filetools.find_file_none(
+                    kwargs["carbon_img_dir"], f"*{protect_area_tile}*.tif"
+                )
+                if carbon_img_file is None:
+                    raise Exception("No Carbon Image file: {}".format(protect_area_tile))
+
+                pxl_area_img_file = rsgislib.tools.filetools.find_file_none(
+                    kwargs["pxl_area_dir"], f"*{protect_area_tile}*.kea"
+                    )
+                if pxl_area_img_file is None:
+                    raise Exception("No reference file: {}".format(protect_area_tile))
+
+                roi_img = os.path.join(
+                    roi_dir, "{}_protect_area_extent.kea".format(protect_area_tile)
+                    )
+
+                out_file = os.path.join(
+                    out_dir, "{}_{}.json".format(protect_area_tile, kwargs["out_file_name"])
+                )
+                if not os.path.exists(out_file):
+                    c_dict = dict()
+                    c_dict["carbon_img"] = carbon_img_file
+                    c_dict["pxl_area"] = pxl_area_img_file
+                    c_dict["roi_img"] = roi_img
+                    c_dict["out_file"] = out_file
+                    self.params.append(c_dict)
+
+    def run_gen_commands(self):
+
+        self.gen_command_info(
+            vec_protect_areas_file="/home/pete/Documents/gmw_protected_areas/data/protected_areas/WDPA-July22-PA_DEF-STATUS-MANGROVE_ind_sites.gpkg",
+            tile_lut_file="/home/pete/Documents/gmw_protected_areas/data/gmw_srtm_tiles_lut.json",
+            carbon_img_dir="/home/pete/Documents/gmw_protected_areas/data/gmw_carbon_data/2018_2020_total_soc",
+            pxl_area_dir="/home/pete/Documents/gmw_protected_areas/data/gmw_srtm_agb_hchm/pxl_areas",
+            out_file_name="soil_c",
+            out_dir_name="soil_c_tile_stats",
+            out_path="/home/pete/Documents/gmw_protected_areas/data/gmw_srtm_protect_areas",
+        )
+
+        self.gen_command_info(
+            vec_protect_areas_file="/home/pete/Documents/gmw_protected_areas/data/protected_areas/WDPA-July22-PA_DEF-STATUS-MANGROVE_ind_sites.gpkg",
+            tile_lut_file="/home/pete/Documents/gmw_protected_areas/data/gmw_srtm_tiles_lut.json",
+            carbon_img_dir="/home/pete/Documents/gmw_protected_areas/data/gmw_carbon_data/2018_2020_total_c",
+            pxl_area_dir="/home/pete/Documents/gmw_protected_areas/data/gmw_srtm_agb_hchm/pxl_areas",
+            out_file_name="total_c",
+            out_dir_name="total_c_tile_stats",
+            out_path="/home/pete/Documents/gmw_protected_areas/data/gmw_srtm_protect_areas",
+            )
+
+        self.gen_command_info(
+            vec_protect_areas_file="/home/pete/Documents/gmw_protected_areas/data/protected_areas/WDPA-July22-PA_DEF-STATUS-MANGROVE_ind_sites.gpkg",
+            tile_lut_file="/home/pete/Documents/gmw_protected_areas/data/gmw_srtm_tiles_lut.json",
+            carbon_img_dir="/home/pete/Documents/gmw_protected_areas/data/gmw_carbon_data/2018_2020_total_co2",
+            pxl_area_dir="/home/pete/Documents/gmw_protected_areas/data/gmw_srtm_agb_hchm/pxl_areas",
+            out_file_name="total_co2",
+            out_dir_name="total_co2_tile_stats",
+            out_path="/home/pete/Documents/gmw_protected_areas/data/gmw_srtm_protect_areas",
+            )
+
+        self.pop_params_db()
+        self.create_shell_exe(
+            run_script="run_exe_analysis.sh",
+            cmds_sh_file="cmds_lst.sh",
+            n_cores=50,
+            db_info_file="pbpt_db_conn_info.json",
+        )
+
+
+if __name__ == "__main__":
+    py_script = os.path.abspath("perform_analysis.py")
+    script_cmd = "python {}".format(py_script)
+
+    process_tools_mod = "perform_analysis"
+    process_tools_cls = "PerformAnalysis"
+
+    create_tools = GenTaskCmds(
+        cmd=script_cmd,
+        db_conn_file="/home/pete/.pbpt_db_conn.txt",
+        lock_file_path="./gmw_lock_file.txt",
+        process_tools_mod=process_tools_mod,
+        process_tools_cls=process_tools_cls,
+    )
+    create_tools.parse_cmds()
---
+++ 10_calc_carbon_stats/01_calc_tile_stats/perform_analysis.py
@@ -0,0 +1,88 @@
+from pbprocesstools.pbpt_q_process import PBPTQProcessTool
+import logging
+import os
+import rsgislib
+import rsgislib.tools.utils
+import numpy
+import osgeo.gdal as gdal
+
+logger = logging.getLogger(__name__)
+
+
+def calc_unq_val_pxl_areas(vals_img, pix_area_img, uid_img, unq_val_area_lut):
+    img_vals_ds = gdal.Open(vals_img)
+    if img_vals_ds is None:
+        raise Exception("Could not open the values input image: '{}'".format(uid_img))
+    img_vals_band = img_vals_ds.GetRasterBand(1)
+    if img_vals_band is None:
+        raise Exception("Failed to read the values image band: '{}'".format(uid_img))
+    vals_arr = img_vals_band.ReadAsArray()
+    img_vals_ds = None
+
+    img_uid_ds = gdal.Open(uid_img)
+    if img_uid_ds is None:
+        raise Exception("Could not open the UID input image: '{}'".format(uid_img))
+    img_uid_band = img_uid_ds.GetRasterBand(1)
+    if img_uid_band is None:
+        raise Exception("Failed to read the UID image band: '{}'".format(uid_img))
+    uid_arr = img_uid_band.ReadAsArray()
+    img_uid_ds = None
+
+    img_pixarea_ds = gdal.Open(pix_area_img)
+    if img_pixarea_ds is None:
+        raise Exception("Could not open the pixel area input image: '{}'".format(pix_area_img))
+    img_pixarea_band = img_pixarea_ds.GetRasterBand(1)
+    if img_pixarea_band is None:
+        raise Exception("Failed to read the pixel area image band: '{}'".format(pix_area_img))
+    pxl_area_arr = img_pixarea_band.ReadAsArray()
+    img_pixarea_ds = None
+
+    msk = numpy.zeros_like(uid_arr, dtype=bool)
+    msk[(uid_arr == 1) & (uid_arr > 0) & (vals_arr > 0)] = True
+
+    if numpy.sum(msk) > 0:
+        unq_val_area_lut['count'] = numpy.sum(msk)
+        unq_val_area_lut['area'] = numpy.sum(pxl_area_arr[msk])
+        unq_val_area_lut['carbon'] = numpy.sum(vals_arr[msk])
+        unq_val_area_lut['carbon_area'] = numpy.sum(vals_arr[msk] * pxl_area_arr[msk])
+
+        vals_unq_val_arr = vals_arr[msk].flatten()
+        vals_hist, bin_edges = numpy.histogram(vals_unq_val_arr, bins=201, range=(0, 5025))
+        unq_val_area_lut['hist'] = unq_val_area_lut['hist'] + vals_hist
+
+class PerformAnalysis(PBPTQProcessTool):
+    def __init__(self):
+        super().__init__(cmd_name="perform_analysis.py", descript=None)
+
+    def do_processing(self, **kwargs):
+        lut_vals = dict()
+        lut_vals['count'] = 0
+        lut_vals['area'] = 0.0
+        lut_vals['carbon'] = 0.0
+        lut_vals['carbon_area'] = 0.0
+        lut_vals['hist'] = numpy.zeros((201), dtype=numpy.uint32)
+
+        calc_unq_val_pxl_areas(self.params['carbon_img'], self.params['pxl_area'], self.params['roi_img'], lut_vals)
+
+        lut_vals['count'] = int(lut_vals['count'])
+        lut_vals['area'] = float(lut_vals['area'])
+        lut_vals['carbon'] = float(lut_vals['carbon'])
+        lut_vals['carbon_area'] = float(lut_vals['carbon_area'])
+
+        rsgislib.tools.utils.write_dict_to_json(lut_vals, self.params['out_file'])
+
+    def required_fields(self, **kwargs):
+        return ["carbon_img", "pxl_area", "roi_img", "out_file"]
+
+    def outputs_present(self, **kwargs):
+        files_dict = dict()
+        files_dict[self.params["out_file"]] = "file"
+        return self.check_files(files_dict)
+
+    def remove_outputs(self, **kwargs):
+        if os.path.exists(self.params["out_file"]):
+            os.remove(self.params["out_file"])
+
+
+if __name__ == "__main__":
+    PerformAnalysis().std_run()

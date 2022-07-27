a41c6a25871bfa164e459970478dbd38d12fb704
Pete Bunting <petebunting@mac.com>
2022-07-27 13:25:07 +0100

    Update to merge carbon columns into vector layer.

--- 11_join_to_vec_lyr/join_stats_to_vec_lyr.py
+++ 11_join_to_vec_lyr/join_stats_to_vec_lyr.py
@@ -9,12 +9,15 @@ wpda_gpdf = wpda_gpdf.set_index("WDPAID")
 
 extent_stats_file = "../06_merge_extent_stats/protected_area_v3_corrected_ext_stats.feather"
 agb_hgt_stats_file = "../09_merge_agb_hgt_stats/protected_agb_hgt_summarised_base_stats.feather"
+carbon_stats_file = "../10_calc_carbon_stats/02_merge_tile_stats/protected_carbon_summarised_base_stats.feather"
 
 extent_stats_df = pandas.read_feather(extent_stats_file)
 agb_hgt_stats_df = pandas.read_feather(agb_hgt_stats_file)
+carbon_stats_df = pandas.read_feather(carbon_stats_file)
 
 wpda_gpdf_join = wpda_gpdf.join(extent_stats_df.set_index("WDPAID"), on="WDPAID")
 wpda_gpdf_join = wpda_gpdf_join.join(agb_hgt_stats_df.set_index("WDPAID"), on="WDPAID")
+wpda_gpdf_join = wpda_gpdf_join.join(carbon_stats_df.set_index("WDPAID"), on="WDPAID")
 
 wpda_gpdf_join.to_file("WDPA_July22_PA_DEF_STATUS_MANGROVE_GMW_Stats.gpkg", layer="WDPA_July22_PA_DEF_STATUS_MANGROVE_GMW_Stats", driver="GPKG")
 
